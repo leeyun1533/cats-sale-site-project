476af5aeacbb4cfb5445a9b481c9338cb2dadadc
이정윤 <leeyun1533@naver.com>
2019-01-12 15:46:26 +0900

    Import dummy data to main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      catList: []
+    }
+  }
+  
+  componentDidMount () {
+    this.getList()
+  }
+  async getList() {
+    const apiUrl = 'dummy/cats.json';
+
+    const { data } = await axios.get(apiUrl)
+    this.setState({
+      catList: data
+    })
+    console.log(this.state.catList[0])
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <span>관리자님 어서오세요.</span>
         </header>
       </div>
     );
